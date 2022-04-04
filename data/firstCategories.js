fb1611bec612fab31ebe2cca80e0468eb3f0dff5
Dev-Choi <>
2022-04-04 15:13:16 +0900

    [데이터 추가] firstCategory, secondCategory 데이터 추가

---
+++ data/firstCategories.js
@@ -0,0 +1,14 @@
+const firstCategories = [
+  {id: "att9AbPxrUSN4vgZ9", name: "맛집"},
+  {id: "rec1nohULLWQVqXZD", name: "카페"},
+  {id: "firstCatGClass", name: "젬 클래스"},
+  {id: "firstCatGCuration", name: "젬 큐레이션"},
+  {id: "rectbHPCk3LZzfu6w", name: "호프주점"},
+  {id: "recTQQfv194uE5O1K", name: "운동헬스"},
+  {id: "recse3cQXji2BWlNu", name: "미용뷰티"},
+  {id: "recqOzD4oznYyHeXS", name: "사진"},
+  {id: "rec9dQcEElsgcAsD7", name: "반려동물"},
+  {id: "recoF9HKKfA3ZSxSP", name: "꽃"},
+  {id: "recXj7vVqBBJttGd8", name: "키즈"},
+  {id: "recz1ftDhPT8VADjf", name: "교육학원"},
+];
