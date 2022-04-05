825089f2887a82b1513adfd5a02345b0668a1a05
Dev-Choi <>
2022-04-05 10:31:43 +0900

    [환경구축] 기본적인 테스팅 환경 구축 완료

--- __tests__/Fire-test.js
+++ __tests__/Fire-test.js
@@ -1,14 +1,15 @@
 import React from "react";
-import renderer from "react-test-renderer";
+import "react-native";
+import {render, fireEvent} from "@testing-library/react-native";
 import FireTest from "../FireTest";
 
 describe("파이어테스트", () => {
   test("화면이 정상적으로 들어오는가", () => {
-    const screen = renderer.create(<FireTest />).toJSON();
+    const screen = render(<FireTest />);
     expect(screen).toMatchSnapshot();
   });
   test("타이틀이 정상적으로 보여지는가", () => {
-    const screen = renderer.create(<FireTest />).toJSON();
+    const screen = render(<FireTest />);
     const title = screen.getByText("Practice TDD");
     expect(title).toBeDefined();
   });
